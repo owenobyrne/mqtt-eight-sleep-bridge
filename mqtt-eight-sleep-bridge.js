df058d0d527f6e8a677ab361b26fad381387c223
Owen O Byrne <owen.obyrne@iol.ie>
2018-08-30 23:41:26 +0100

    Added ability to turn the eight on/off using mqtt messaging

--- mqtt-eight-sleep-bridge.js
+++ mqtt-eight-sleep-bridge.js
@@ -10,12 +10,16 @@ require('homeautomation-js-lib/mqtt_helpers.js')
 const username = process.env.USERNAME
 const password = process.env.PASSWORD
 var pollTime = process.env.POLL_FREQUENCY
+const mqttHost = process.env.MQTT_HOST
+const mqttUsername = process.env.MQTT_USERNAME
+const mqttPassword = process.env.MQTT_PASSWORD
 
 if ( _.isNil(pollTime) ) {
-    pollTime = 5
+    pollTime = 30
 }
 
 var shouldRetain = process.env.MQTT_RETAIN
+var deviceId = "";
 
 if (_.isNil(shouldRetain)) {
     shouldRetain = true
@@ -35,16 +39,38 @@ if (_.isNil(baseTopic)) {
     process.abort()
 }
 
-var connectedEvent = function() {
-    health.healthyEvent()
-}
-
-var disconnectedEvent = function() {
-    health.unhealthyEvent()
-}
-
 // Setup MQTT
-var client = mqtt.setupClient(connectedEvent, disconnectedEvent)
+const mqttClient  = mqtt.connect('mqtt://' + mqttHost, { username: mqttUsername, password: mqttPassword })
+
+mqttClient.on('connect', () => {
+    mqttClient.subscribe('eight/#')
+})
+
+mqttClient.on('message', (topic, message) => {
+    var command = {}
+    
+    if (topic === baseTopic + '/left/setlevel') {
+        command = { leftTargetHeatingLevel: _.parseInt(message.toString()) }
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    } else if (topic === baseTopic + '/right/setlevel') {
+        command = { rightTargetHeatingLevel: _.parseInt(message.toString()) }
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    } else if (topic === baseTopic + '/left/switch/on') {
+        command = { leftHeatingDuration: 3600 };
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    } else if (topic === baseTopic + '/left/switch/off') {
+        command = { leftHeatingDuration: 0 };
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    } else if (topic === baseTopic + '/right/switch/on') {
+        command = { rightHeatingDuration: 3600 };
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    } else if (topic === baseTopic + '/right/switch/off') {
+        command = { rightHeatingDuration: 0 };
+        eight.setDeviceHeatingLevel(deviceId, command, function(err, device) {})
+    }
+
+    
+})
 
 eight.start(username, password, pollTime)
 
@@ -54,10 +80,13 @@ eight.on('updated', (result) => {
     
     Object.keys(result).forEach(key => {
         const value = result[key]
+
+        if (key === "deviceId") { deviceId = value }
+
         logging.debug('   base: ' + key + ': ' + value)
         var topic = baseTopic + '/' + key
 
-        client.smartPublish(topic, JSON.stringify(value).toString(), mqttOptions)
+        mqttClient.publish(topic, JSON.stringify(value).toString(), mqttOptions)
     })
 })
 
@@ -66,14 +95,14 @@ eight.on('user-updated', (user) => {
 
     var topic = baseTopic + '/' + user.name() + '/'
 
-    client.smartPublish(topic + 'nowHeating', user.nowHeating().toString(), mqttOptions)
-    client.smartPublish(topic + 'present', user.present().toString(), mqttOptions)
-    client.smartPublish(topic + 'heatingLevel', user.heatingLevel().toString(), mqttOptions)
-    client.smartPublish(topic + 'userId', user.userId().toString(), mqttOptions)
-    client.smartPublish(topic + 'heatingDuration', user.heatingDuration().toString(), mqttOptions)
-    client.smartPublish(topic + 'targetHeatingLevel', user.targetHeatingLevel().toString(), mqttOptions)
-    client.smartPublish(topic + 'presenceEnd', user.presenceEnd().toString(), mqttOptions)
-    client.smartPublish(topic + 'presenceStart', user.presenceStart().toString(), mqttOptions)
+    mqttClient.publish(topic + 'nowHeating', user.nowHeating().toString(), mqttOptions)
+    mqttClient.publish(topic + 'present', user.present().toString(), mqttOptions)
+    mqttClient.publish(topic + 'heatingLevel', user.heatingLevel().toString(), mqttOptions)
+    mqttClient.publish(topic + 'userId', user.userId().toString(), mqttOptions)
+    mqttClient.publish(topic + 'heatingDuration', user.heatingDuration().toString(), mqttOptions)
+    mqttClient.publish(topic + 'targetHeatingLevel', user.targetHeatingLevel().toString(), mqttOptions)
+    mqttClient.publish(topic + 'presenceEnd', user.presenceEnd().toString(), mqttOptions)
+    mqttClient.publish(topic + 'presenceStart', user.presenceStart().toString(), mqttOptions)
 
 })
 
